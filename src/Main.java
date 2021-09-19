83e154d4f3735c237b0de360da1ddf5aff16d1a7
Marcos Alvarez <alvmivan@gmail.com>
2021-09-18 21:20:30 -0300

    inicializo el proye

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import arrayAlgorithms.SpiralIteration;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hola Mundo");
+        SpiralIteration.test();
+    }
+}
